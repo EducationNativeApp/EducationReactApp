e02ebe682bdfbaf878d09bae6acc24708b4dd9b6
123khouloud <kouelhazi@yahoo.com>
2023-10-01 11:40:53 +0100

    signup

--- frontEnd/my-app/App.js
+++ frontEnd/my-app/App.js
@@ -3,17 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './screens/LoginPage/Login/Login'
+ import Login from './screens/LoginPage/Login/Login'
 import SignUp from './screens/LoginPage/SignUp/SignUp';
 const Stack = createNativeStackNavigator()
+
 
 export default function App() {
   return (
     <NativeBaseProvider>
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="SignUp">
     <Stack.Screen name="Login" component={Login}  />
-    <Stack.Screen name="SignUp" component={SignUp}  />
+    <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false}}  />
     </Stack.Navigator>
     </NavigationContainer>
     </NativeBaseProvider>
